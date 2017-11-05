17ff4422fdf58e70d3538f065b4acef0ee673746
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-05 09:56:26 -0500

    talking about classes

--- classes.py
+++ classes.py
@@ -1,4 +1,29 @@
 # Classes
 class Greeter:
+    # ALL CLASSES HAVE TO HAVE SELF SET
     # This line is required:
-    def __init__(self):
+    def __init__(self, greeting="Hello", excited=True):
+        # "greeting" does not have to be set, but if it is, it will
+        # use what it is set as by default if nothing is specified
+        self.greeting = greeting
+        self.excited = excited
+        # Also, any variables in init must be declared like above
+
+    def greet(self, name):
+        punct = "."
+        if self.excited:
+            punct = "!"
+        print(self.greeting + " " + name + "!")
+
+# Now, the classes "self" must be set...
+english_greeter = Greeter()
+spanish_greeter = Greeter("Hola", False)
+french_greeter = Greeter(excited=True, greeting="Bonjour")
+
+# ...And Called
+english_greeter.greet("Remy")
+spanish_greeter.greet("Remy")
+french_greeter.greet("Remy")
+
+# Or if you want to take a shortcut
+Greeter("Hello").greet("Shorty")
