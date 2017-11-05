b0d382f6d4b4daac91f647641e5d312833e0f283
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-05 09:30:33 -0500

    elseif

--- examplefirst.py
+++ examplefirst.py
@@ -2,14 +2,12 @@ a = 1
 
 # Basic If Statement
 if a == 1:
-    print("A is double equal to 1")
+    print("A is equal to 1")
+# else if
+elif a == 2:
+    print("A is equal to 2")
+# else
 else:
-    print("A is NOT equal to 1")
-
-# Else if
-if a == 2:
-    print("A is double equal to 2")
-else:
-    print("A is NOT equal to 2")
+    print("A is NOT 1 or 2")
 
 print("endofline")
