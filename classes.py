b96c12f4f701bc61b9cf22b673c19f934a4f1847
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-05 10:08:25 -0500

    Add notes

--- classes.py
+++ classes.py
@@ -1,12 +1,12 @@
 # Classes
 class Greeter:
-    # ALL CLASSES HAVE TO HAVE SELF SET
+    # ALL CLASSES HAVE TO HAVE "SELF" SET
     # This line is required:
     def __init__(self, greeting="Hello", excited=True):
         # "greeting" does not have to be set, but if it is, it will
         # use what it is set as by default if nothing is specified
-        self.greeting = greeting
-        self.excited = excited
+        self.greeting = greeting # Sets greeting to use
+        self.excited = excited # boolean for punctuation
         # Also, any variables in init must be declared like above
 
         # ALWAYS REMEMBER to use self below, or else you will get a "this takes two arguments" error
@@ -18,8 +18,8 @@ class Greeter:
 
 # Now, the classes "self" must be set...
 english_greeter = Greeter()
-spanish_greeter = Greeter("Hola", False)
-french_greeter = Greeter(excited=False, greeting="Bonjour")
+spanish_greeter = Greeter("Hola", False) # You can call it in order of the init...
+french_greeter = Greeter(excited=False, greeting="Bonjour") # ...or set them manually
 
 # ...And Called
 english_greeter.greet("Remy")
@@ -28,3 +28,10 @@ french_greeter.greet("Remy")
 
 # Or if you want to take a shortcut
 Greeter("Hello").greet("Shorty")
+
+
+# Multi-line String
+multi_line_string = """
+Hello!
+"""
+print(multi_line_string)
