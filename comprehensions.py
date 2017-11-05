469581cddde3e660a334a808494a15c74ab194a7
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-05 10:32:09 -0500

    reconfigure loops to be more efficient

--- comprehensions.py
+++ comprehensions.py
@@ -1,20 +1,22 @@
 wordslist = ["File", "apple", "directory", "bannana"]
 
+
 def reverseword(word):
     return word[::-1]
 
-for word in wordslist:
-        print(reverseword(word))
-
 reversed_words = []
 for word in wordslist:
-        reversed_words.append(reverseword(word))
+    reversed_words.append(reverseword(word))
+    print(reverseword(word))
 print(reversed_words)
+
 
 reversed_words = [reverseword(word) for word in wordslist]
 word_lengths = [len(word) for word in wordslist]
 new_word_list = [word for word in wordslist]
 
+
+# inside of the brackets, the whitespace does not matter
 short_word_list = [word for word in wordslist if len(word) < 7]
 print ("short words: ", short_word_list)
 
