fcc032a82c1769c22c47496ac1f1fab10cf4b8d4
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-05 09:29:04 -0500

    initial commit

---
+++ examplefirst.py
@@ -0,0 +1,15 @@
+a = 1
+
+# Basic If Statement
+if a == 1:
+    print("A is double equal to 1")
+else:
+    print("A is NOT equal to 1")
+
+# Else if
+if a == 2:
+    print("A is double equal to 2")
+else:
+    print("A is NOT equal to 2")
+
+print("endofline")
